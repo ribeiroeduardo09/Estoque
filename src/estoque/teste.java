32960bdeccb51cc34ebb55a559bbb9a1933acde7
Eduardo Pereira Ribeiro <eduardo.ribeiro25@fatec.sp.gov.br>
2017-05-05 19:30:42 -0300

    Teste

--- src/estoque/teste.java
+++ src/estoque/teste.java
@@ -7,9 +7,21 @@ public class Teste {
 	
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto("Teclado", 17.50f, 20.50f, 111111, "Teclado comum", 3, LocalDate.of(2017, Month.FEBRUARY, 10));
+		Sistema sist = new Sistema();
 		
+		Produto p1 = new Produto("Teclado", 15.50f, 20.50f, 111111, "Teclado comum", 3, LocalDate.of(2017, Month.FEBRUARY, 10));
+		Produto p2 = new Produto("Monitor 15 Pol.", 234.90f, 310.50f, 222222, "Monitor LG 15 Polegadas", 5, LocalDate.of(2017, Month.FEBRUARY, 28));
 		
+		sist.addProduto(p1);
+		sist.addProduto(p2);
+		
+		Produto produtoBuscado = sist.searchProduto(222222);
+		
+		if(produtoBuscado!=null){
+			System.out.println(produtoBuscado.getNome());
+		} else {
+			System.out.println("Não encontrado.");
+		}
 		
 	}
 
