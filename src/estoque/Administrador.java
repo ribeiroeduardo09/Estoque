f3e514dcd7813d1d0d27329242ebadcba04ed659
filiperdt <rubenfilipeao@gmail.com>
2017-05-16 20:45:43 -0300

    Criação das classes Administrador e Fornecedor

--- src/estoque/Administrador.java
+++ src/estoque/Administrador.java
@@ -1,5 +1,31 @@
 package estoque;
 
 public class Administrador {
-
+	
+	private String login;
+	private String senha;
+	
+	public Administrador(String login, String senha)
+	{
+		this.login = login;
+		this.senha = senha;
+	}
+	
+	public String getLogin()
+	{
+		return login;
+	}
+	public void setLogin(String novoLogin)
+	{
+		this.login = novoLogin;
+	}
+	
+	public String getSenha()
+	{
+		return senha;
+	}
+	public void setSenha(String novaSenha)
+	{
+		this.senha = novaSenha;
+	}
 }
